c2d30962739f5eced8bd3298703f2022d5f53750
Aleksandr Prokopev <avprokopev@avito.ru>
2023-02-27 10:51:31 +0600

    connect postgresql

--- functions/packages/finlog/test/index.js
+++ functions/packages/finlog/test/index.js
@@ -1,6 +1,18 @@
-function main(args) {
-    let name = args.name || 'stranger'
-    let greeting = 'Hello ' + name + '!'
-    console.log(greeting)
-    return {"body": greeting}
+const { Pool } = require('pg')
+
+async function main(args) {
+    const pool = new Pool({
+        connectionString: process.env.POSTGRES_URL,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD
+    })
+
+    try {
+        const res = await pool.query('select * from accounts')
+        console.log(res.rows)
+        return res.rows
+    } catch (err) {
+        console.error(err)
+        throw err
+    }
 }

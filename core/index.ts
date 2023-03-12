dfbff01d1e48b2a7edbf31df234dd9d5d24aea0e
Aleksandr Prokopev <avprokopev@avito.ru>
2023-03-12 09:14:55 +0600

    clean up the project after experiments

--- core/index.ts
+++
@@ -1,7 +0,0 @@
-export { connectDatabase } from './src/storage/connect'
-export { UploadsStorage } from './src/storage/uploads'
-
-export { createLogger } from './src/utils/logger'
-export { CommandError } from './src/utils/error'
-
-export { UploadHandler } from './src/handlers/upload'
---
+++ core/src/index.ts
@@ -0,0 +1,7 @@
+export { connectDatabase } from './storage/connect'
+export { UploadsStorage } from './storage/uploads'
+
+export { createLogger } from './utils/logger'
+export { CommandError } from './utils/error'
+
+export { UploadHandler } from './handlers/upload'
